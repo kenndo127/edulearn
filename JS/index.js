fbdc0c56984c20141df40cd6ca6230981694fe9d
Kenneth Okechukwu <kennethchidiebube5@gmail.com>
2026-02-10 15:12:59 +0100

    Done with menu.

---
+++ JS/index.js
@@ -0,0 +1,8 @@
+function showMenu(){
+  const menu = document.querySelector("#menuToggler");
+  menu.style.display = 'flex';
+}
+function hideMenu(){
+  const menu = document.querySelector("#menuToggler");
+  menu.style.display = 'none';
+}
