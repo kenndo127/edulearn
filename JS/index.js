aa955b8198ff78d7706aa57f3dd74b778b733cb7
Kenneth Okechukwu <kennethchidiebube5@gmail.com>
2026-02-11 16:13:02 +0100

    Owl carousel width is stressing me.

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,4 @@
+//Landing page side nav functions
 function showMenu(){
   const menu = document.querySelector("#menuToggler");
   menu.style.display = 'flex';
@@ -6,3 +7,28 @@ function hideMenu(){
   const menu = document.querySelector("#menuToggler");
   menu.style.display = 'none';
 }
+
+//Owl Carousel Initializer
+$(document).ready(function() {
+ 
+  $(".owl-carousel").owlCarousel({
+    // loop:true,
+    // margin:20,      // spacing between cards
+    // nav:true,
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     576:{
+    //         items:2
+    //     },
+    //     768:{
+    //         items:3
+    //     },
+    //     992:{
+    //         items:4
+    //     }
+    // }
+  });
+
+});
